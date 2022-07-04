d59fe90cbbb09fe97758067af263b9ca3a21b639
hafizjavaid <hafizjaidi786@gmail.com>
2022-07-04 22:22:27 +0500

    Done with responsive-cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
 function App() {
+  const [cards] = useState([
+    {
+      title: "Card-1",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta nam vero sapiente eius fugit distinctio
+      pariatur? A, molestias.`,
+    },
+    {
+      title: "Card-2",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta`,
+    },
+    {
+      title: "Card-3",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta dolor sit amet consectetur adipisicing elit.`,
+    },
+    {
+      title: "Card-4",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta`,
+    },
+    {
+      title: "Card-5",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta nam vero sapiente eius fugit distinctio
+      pariatur? A, molestias.`,
+    },
+    {
+      title: "Card-6",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      perspiciatis soluta dolor sit amet consectetur adipisicing elit.`,
+    },
+  ]);
   return (
     <div>
-      <h1>Hello World</h1>
+      <section>
+        <div className="container">
+          <h1>Responsive Cards</h1>
+          <div className="cards">
+            {cards.map((card, i) => (
+              <div key={i} className="card">
+                <h3>{card.title}</h3>
+                <p>{card.text}</p>
+                <button className="btn">Explore</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
